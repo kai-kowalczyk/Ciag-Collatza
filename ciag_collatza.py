3ba1dcde416dee7205ddbeb58cfba0763b4625df
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-08-27 12:41:39 +0200

    final version of the code

--- ciag_collatza.py
+++ ciag_collatza.py
@@ -1,3 +1,17 @@
-liczba = int(input())
-if liczba < 1 or liczba > 100:
+liczba = int(input('Podaj liczbę od 1 do 100: '))
+dlugosc_ciagu = 0
+
+while liczba < 1 or liczba > 100: 
     print('Liczba wykracza poza zakres 1-100, podaj liczbę jeszcze raz: ')
+    liczba = int(input())
+dlugosc_ciagu += 1
+pierwsza_liczba = liczba
+while liczba != 1:
+    if liczba % 2 == 0:
+        liczba = liczba / 2
+        dlugosc_ciagu += 1
+    else:
+        liczba = 3 * liczba + 1
+        dlugosc_ciagu += 1
+print(f'Długość ciągu Collatza dla {pierwsza_liczba}: {dlugosc_ciagu}')
+    
